edc15d8a409fd87f857a56e3822fcfea4dc92720
Lais Ziegler <laisziegler@gmail.com>
2021-10-10 13:17:45 -0300

    methods update

---
+++ src/main/java/com/bootcamp/dio/restfull/controller/request/SoldadoEditRequest.java
@@ -0,0 +1,32 @@
+package com.bootcamp.dio.restfull.controller.request;
+
+public class SoldadoEdit {
+
+    private String nome;
+    private String raca;
+    private String arma;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getArma() {
+        return arma;
+    }
+
+    public void setArma(String arma) {
+        this.arma = arma;
+    }
+}
